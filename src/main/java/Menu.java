d2dd7d35231731d85e5dbd10599dad369306f6d5
Toritou <l.toro05@ufromail.cl>
2024-04-25 10:30:44 -0400

    menú con algunos casos completados el archivo de perosna está listo

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,2 +1,43 @@
 public class Menu {
+    public void menu() {
+        Scanner = new scanner(System.in);
+        String opcion;
+        do {
+            System.out.println(" /n LAVADO LOCOSHÓN ");
+            System.out.println("1. Crear Orden");
+            System.out.println("2. Mostrar Ordenes");
+            System.out.println("3. Completar Ordenes");
+            System.out.println("4. Mostrar Ordenes Finalizadas");
+            System.out.println("5. Salir");
+            System.out.println("Ingrese una opcion: ");
+            opcion = sc.nextLine();
+            switch (opcion) {
+                case "1":
+                    Persona persona = Persona.crearPersona();
+                    System.out.println("Ingrese tipo de lavado: ");
+                    String tipoLav = sc.nextLine();
+                    System.out.println("Ingrese precio: ");
+                    int precio = sc.nextInt();
+                    Lavado lavado = new Lavado(tipoLav, precio);
+                    Orden orden = new Orden(persona, lavado);
+                    break;
+                case "2":
+                    Orden.mostrarOrdenes();
+                    break;
+                case "3":
+                    Orden.completarOrden();
+                    break;
+                case "4":
+                    Orden.mostrarOrdenesFinalizadas();
+                    break;
+                case "5":
+                    System.out.println("Saliendo...");
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+            while (!opcion.equals("5")) ;
+        }
+    }
 }
--- src/main/java/Persona.java
+++ src/main/java/Persona.java
@@ -1,2 +1,88 @@
+import java.util.Scanner;
+
 public class Persona {
+        private String nombre;
+        private String rut;
+        private String telefono;
+        private String patente;
+        private String modelo;
+
+        //Constructor vacio pera crear la persona//
+        public Persona() {
+        }
+        //Constructor//
+        public Persona(String nombre, String rut, String telefono, String patente, String modelo) {
+            this.nombre = nombre;
+            this.rut = rut;
+            this.telefono = telefono;
+            this.patente = patente;
+            this.modelo = modelo;
+        }
+
+        //Metodos getters//}
+        public String getNombre() {
+            return nombre;
+        }
+
+        public String getRut() {
+            return rut;
+        }
+
+        public String getTelefono() {
+            return telefono;
+        }
+
+        public String getPatente() {
+            return patente;
+        }
+
+        public String getModelo() {
+            return modelo;
+        }
+
+    //Metodos setters//
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+        public void setRut(String rut) {
+            this.rut = rut;
+        }
+        public void setTelefono(String telefono) {
+            this.telefono = telefono;
+        }
+
+        public void setPatente(String patente) {
+            this.patente = patente;
+        }
+        public void setModelo(String modelo) {
+            this.modelo = modelo;
+        }
+
+        //Metodo toString//
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", rut='" + rut + '\'' +
+                ", telefono='" + telefono + '\'' +
+                ", patente='" + patente + '\'' +
+                ", modelo='" + modelo + '\'' +
+                '}';
+    }
+
+    //Crear persona//
+    public static Persona crearPersona() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese nombre: ");
+        String nombre = sc.nextLine();
+        System.out.println("Ingrese rut: ");
+        String rut = sc.nextLine();
+        System.out.println("Ingrese telefono: ");
+        String telefono = sc.nextLine();
+        System.out.println("Ingrese patente: ");
+        String patente = sc.nextLine();
+        System.out.println("Ingrese modelo: ");
+        String modelo = sc.nextLine();
+        return new Persona(nombre, rut, telefono, patente, modelo);
+    }
 }
